145812de68c89ccad317720082259a31b4acf73f
Shay Reichert <shay.reichert@outlook.fr>
2023-08-12 11:22:39 +0200

    Menu : moving bar

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./NavBar.module.scss";
@@ -12,6 +12,8 @@ export default function NavBar() {
   const pathname = usePathname();
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const menuRef = useRef<HTMLUListElement | null>(null);
+  const [movingBarPosition, setMovingBarPosition] = useState({ width: 0, left: 0 });
+  const [isBarPendingUpdate, setIsBarPendingUpdate] = useState(false);
 
   const menuItems = [
     { id: "services", label: "Offres et Services", path: "/offres-et-services" },
@@ -21,6 +23,19 @@ export default function NavBar() {
     { id: "blog", label: "Blog", path: "/blog" },
   ];
 
+  useEffect(() => {
+    // Clean state when an update of the moving bar is completed
+    if (!isBarPendingUpdate) {
+      const currentMenuItem = menuRef.current?.querySelector(`.${styles["current"]}`) as HTMLElement;
+      if (currentMenuItem) {
+        setMovingBarPosition({
+          width: currentMenuItem.offsetWidth,
+          left: currentMenuItem.offsetLeft,
+        });
+      }
+    }
+  }, [isBarPendingUpdate]);
+
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
@@ -29,7 +44,7 @@ export default function NavBar() {
     setIsMobileMenuOpen(false);
   };
 
-  // Improve accessibility
+  // Accessibility : menu mobile focus
   const handleTransitionEnd = () => {
     if (isMobileMenuOpen) {
       if (menuRef.current) {
@@ -39,6 +54,40 @@ export default function NavBar() {
         }
       }
     }
+  };
+
+  // Accessibility : menu desktop navigation
+  const handleLinkKeyDown = (e: React.KeyboardEvent<HTMLAnchorElement>) => {
+    if (e.key === "Enter") {
+      const link = e.currentTarget;
+      const width = link.offsetWidth;
+      const left = link.offsetLeft;
+      setMovingBarPosition({ width, left });
+    }
+  };
+
+  const handleBarMouseEnter = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    const link = e.currentTarget;
+    const width = link.offsetWidth;
+    const left = link.offsetLeft;
+    setMovingBarPosition({ width, left });
+  };
+
+  const handleBarMouseLeave = () => {
+    setIsBarPendingUpdate(true);
+    setTimeout(() => {
+      const currentMenuItem = menuItems.find((item) => pathname === item.path);
+      const movingBar = menuRef.current?.querySelector(`.${styles["moving-bar"]}`) as HTMLElement;
+
+      if (currentMenuItem && movingBar) {
+        const menuItemElement = menuRef.current?.querySelector(`.${styles["current"]}`) as HTMLElement;
+        const width = menuItemElement.offsetWidth;
+        const left = menuItemElement.offsetLeft;
+        movingBar.style.width = `${width}px`;
+        movingBar.style.left = `${left}px`;
+      }
+      setIsBarPendingUpdate(false);
+    }, 300);
   };
 
   return (
@@ -53,14 +102,24 @@ export default function NavBar() {
         ref={menuRef}
         className={`${styles["links"]} ${aceSans.className} ${isMobileMenuOpen ? styles["menu-open"] : ""}`}
         onTransitionEnd={handleTransitionEnd}
+        onMouseLeave={handleBarMouseLeave}
       >
         {menuItems.map((item) => (
           <li key={item.id} className={pathname === item.path ? styles["current"] : ""}>
-            <Link href={item.path} aria-current={pathname === item.path ? "page" : undefined} onClick={handleClickMenuItem}>
+            <Link
+              href={item.path}
+              aria-current={pathname === item.path ? "page" : undefined}
+              onClick={handleClickMenuItem}
+              onMouseEnter={handleBarMouseEnter}
+              role="link"
+              tabIndex={0}
+              onKeyDown={handleLinkKeyDown}
+            >
               {item.label}
             </Link>
           </li>
         ))}
+        <span className={styles["moving-bar"]} style={{ width: `${movingBarPosition.width}px`, left: `${movingBarPosition.left}px` }}></span>
       </ul>
 
       <MobileMenuIcon isOpen={isMobileMenuOpen} onClick={toggleMobileMenu} />
