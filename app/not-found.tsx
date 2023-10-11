3b5e1a242e7026f45df260a89a50013c9dcbed69
Shay Reichert <shay.reichert@outlook.fr>
2023-10-11 11:11:05 +0200

    Fixes

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,12 @@
+import Button from "./components/Button/Button";
 import SectionTitle from "./components/SectionTitle/SectionTitle";
+import styles from "./styles/not-found.module.scss";
 
 export default function NotFound() {
   return (
-    <main id="main">
+    <main id="main" className={styles["not-found"]}>
       <SectionTitle data={{ title: "Ooooups !", baseline: "Cette page n'existe pas" }} has_decoration={false} />
+      <Button href="/" text="Retour&nbsp;à&nbsp;l'accueil" />
     </main>
   );
 }
