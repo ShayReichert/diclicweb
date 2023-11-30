a58eb1faa2b07c603741639b3f2026c0b994c9d2
Shay Reichert <shay.reichert@outlook.fr>
2023-11-30 09:16:52 +0100

    Jumbo effect

--- app/components/SectionTitle/SectionTitle.tsx
+++ app/components/SectionTitle/SectionTitle.tsx
@@ -7,6 +7,7 @@ export default function SectionTitle({ data, has_decoration = true, is_long_titl
 
   return (
     <section className={styles["section-title"]}>
+      <div className={styles["jumbo"]}></div>
       <div className={styles["content"]}>
         <div className={styles["image-wrapper"]}>
           <Image
