792d3e852d74bdc845e0e54b85ae6cfee53c97d2
Shay Reichert <shay.reichert@outlook.fr>
2024-02-09 15:49:36 +0100

    Post : undo changes to fix error 500

--- app/blog/[slug]/[id]/page.tsx
+++ app/blog/[slug]/[id]/page.tsx
@@ -1,19 +1,32 @@
-import { fetchPostById } from "@/app/utils/blog";
-import PostContainer from "./components/PostContainer/PostContainer";
+import { fetchPostById, getRandomRelatedPosts } from "@/app/utils/blog";
+import SectionBlogTitle from "./components/SectionBlogTitle/SectionBlogTitle";
+import SectionCatchPhrase from "@/app/components/SectionCatchPhrase/SectionCatchPhrase";
+import SectionContent from "./components/SectionContent/SectionContent";
+import SectionRelatedPosts from "./components/SectionRelatedPosts/SectionRelatedPosts";
 
 export function generateMetadata({ params: { id } }: PostProps) {
   const post = fetchPostById(parseInt(id));
-
   return {
     title: `Blog | Diclicweb | ${post.title}`,
     description: post.excerpt,
     openGraph: {
-      ...{ images: [`${post.thumbnail.path}`] },
+      ...{ images: [`${post.thumbnail}`] },
     },
   };
 }
 
 export default function Post({ params: { id } }: PostProps) {
   const postId = parseInt(id);
-  return <PostContainer postId={postId} />;
+  const post = fetchPostById(postId);
+  const totalRelatedPosts = 3;
+  const relatedPosts = getRandomRelatedPosts(post.related_posts, totalRelatedPosts, postId);
+
+  return (
+    <main id="main">
+      <SectionBlogTitle title={post.title} />
+      <SectionCatchPhrase data={{ text: post.excerpt }} />
+      <SectionContent data={post} />
+      <SectionRelatedPosts relatedPosts={relatedPosts} />
+    </main>
+  );
 }
