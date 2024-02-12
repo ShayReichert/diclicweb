9a52809bcbc97a50b4e419d5393c12585743048a
Shay Reichert <shay.reichert@outlook.fr>
2024-02-12 15:29:11 +0100

    Smaller logo in title

--- app/blog/[slug]/[id]/components/SectionBlogTitle/SectionBlogTitle.tsx
+++ app/blog/[slug]/[id]/components/SectionBlogTitle/SectionBlogTitle.tsx
@@ -7,7 +7,7 @@ export default function SectionBlogTitle({ title }: SectionBlogTitleProps) {
     <section className={styles["section-title"]}>
       <div className={styles["content"]}>
         <div className={styles["image-wrapper"]}>
-          <Image src="/images/logo_blue.webp" alt="Logo de Diclicweb" width={297} height={209} priority />
+          <Image src="/images/logo_blue.webp" alt="Logo de Diclicweb" width={250} height={175} priority />
         </div>
         {title && (
           <div className={styles["title-wrapper"]}>
--- app/components/SectionTitle/SectionTitle.tsx
+++ app/components/SectionTitle/SectionTitle.tsx
@@ -10,7 +10,7 @@ export default function SectionTitle({ data, has_decoration = true, is_long_titl
       <div className={styles["jumbo"]}></div>
       <div className={styles["content"]}>
         <div className={styles["image-wrapper"]}>
-          <Image src="/images/logo_blue.webp" alt="Logo de Diclicweb" width={297} height={209} priority />
+          <Image src="/images/logo_blue.webp" alt="Logo de Diclicweb" width={250} height={175} priority />
         </div>
         {title && (
           <div className={styles["title-wrapper"]}>
