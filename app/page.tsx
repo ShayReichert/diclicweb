68e083406b00ac87f0353486ce768fa5930cfd56
Shay Reichert <shay.reichert@outlook.fr>
2024-02-16 16:13:33 +0100

    Disable locomotive scroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import SectionTitle from "./components/SectionTitle/SectionTitle";
 import SectionCatchPhrase from "./components/SectionCatchPhrase/SectionCatchPhrase";
 import SectionServices from "./components/SectionServices/SectionServices";
@@ -11,12 +11,12 @@ import { sectionServicesData } from "./_data/entities/services";
 import { sectionProjectsData } from "./_data/entities/projects";
 
 export default function Home() {
-  useEffect(() => {
-    (async () => {
-      const LocomotiveScroll = (await import("locomotive-scroll")).default;
-      const locomotiveScroll = new LocomotiveScroll();
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     const LocomotiveScroll = (await import("locomotive-scroll")).default;
+  //     const locomotiveScroll = new LocomotiveScroll();
+  //   })();
+  // }, []);
 
   return (
     <main id="main">
